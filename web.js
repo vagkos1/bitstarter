c4fcc1ed3df616909bc188cd5c128cb4bf779593
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2013-07-10 13:46:15 +0000

    Hello from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var bufferData = fs.readFileSync(index.html);
+  response.send('bufferData.toString');
 });
 
 var port = process.env.PORT || 5000;
