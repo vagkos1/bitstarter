2ccb51f305c688cbc3bfd05045d1c19b467b4f01
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2013-07-10 13:52:05 +0000

    Hello from index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var bufferData = fs.readFileSync(index.html);
+  var bufferData = fs.readFileSync('index.html');
   response.send('bufferData.toString');
 });
 
